bed5798a86de9790071ac6ddf3fb60085c1aadbd
Emil <emilgth@gmail.com>
2019-11-18 10:37:13 +0100

    changed env connection string

--- src/main/java/utils/EMF_Creator.java
+++ src/main/java/utils/EMF_Creator.java
@@ -123,7 +123,7 @@ public class EMF_Creator {
         if (isDeployed) {
             user = System.getenv("USER");
             pw = System.getenv("PW");
-            connection_str = System.getenv("CONNECTION_STR");
+            connection_str = System.getenv("MARGA_CONNECTION_STR");
         }
         /*
         On your server in /opt/tomcat/bin/setenv.sh   add the following WITH YOUR OWN VALUES
