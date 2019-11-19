5faedc742dfdbe1b50df7ddd74e947858ed14a17
Emil <emilgth@gmail.com>
2019-11-19 16:20:47 +0100

    fixed empty array from getallflights endpoint

--- src/main/java/rest/FlightResource.java
+++ src/main/java/rest/FlightResource.java
@@ -2,6 +2,7 @@ package rest;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import dtos.FlightDTO;
 import facades.FacadeExample;
 import facades.FlightFacade;
 import utils.EMF_Creator;
@@ -11,15 +12,13 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Path("flights")
 public class FlightResource {
 
     private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory(
-            "pu",
-            "jdbc:mysql://localhost:3307/startcode",
-            "dev",
-            "ax2",
+            EMF_Creator.DbSelector.DEV,
             EMF_Creator.Strategy.CREATE);
     private static final FlightFacade FLIGHT_FACADE = FlightFacade.getFlightFacade(EMF);
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
@@ -34,7 +33,8 @@ public class FlightResource {
     @GET
     @Produces({MediaType.APPLICATION_JSON})
     public String getAllFlights() {
-        return GSON.toJson(FLIGHT_FACADE.getAllFlights());
+        List<FlightDTO> flights = FLIGHT_FACADE.getAllFlights();
+        return GSON.toJson(flights);
     }
 
 }
