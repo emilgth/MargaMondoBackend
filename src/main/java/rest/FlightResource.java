bdc43376f4f3467c02cef57c7610666784fa6494
Matias <buekoefoed@gmail.com>
2019-11-21 00:02:53 +0100

    added facade and endpoint for global with threads and queue

--- src/main/java/rest/FlightResource.java
+++ src/main/java/rest/FlightResource.java
@@ -2,9 +2,11 @@ package rest;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import dtos.FlightDTO;
-import facades.FacadeExample;
 import facades.FlightFacade;
+import facades.ScraperFacade;
 import utils.EMF_Creator;
 
 import javax.persistence.EntityManagerFactory;
@@ -13,6 +15,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 @Path("flights")
 public class FlightResource {
@@ -21,6 +24,7 @@ public class FlightResource {
             EMF_Creator.DbSelector.DEV,
             EMF_Creator.Strategy.CREATE);
     private static final FlightFacade FLIGHT_FACADE = FlightFacade.getFlightFacade(EMF);
+    private static final ScraperFacade SCRAPER_FACADE = ScraperFacade.getScraperFacade(EMF);
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
 
     @GET
@@ -37,4 +41,16 @@ public class FlightResource {
         return GSON.toJson(flights);
     }
 
+    @Path("global")
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public String getGlobalFlights() throws ExecutionException, InterruptedException {
+        StringBuilder jsonStr = new StringBuilder();
+        for (String json : SCRAPER_FACADE.getAllApiData()) {
+            jsonStr.append(json);
+        }
+        JsonParser jsonParser = new JsonParser();
+        JsonElement data = jsonParser.parse(jsonStr.toString());
+        return GSON.toJson(data);
+    }
 }
---
+++ src/test/java/facades/ScraperFacadeTest.java
@@ -0,0 +1,35 @@
+package facades;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import utils.EMF_Creator;
+
+import javax.persistence.EntityManagerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+//Uncomment the line below, to temporarily disable this test
+//@Disabled
+class ScraperFacadeTest {
+
+    private static EntityManagerFactory emf;
+    private static ScraperFacade scraperFacade;
+
+    ScraperFacadeTest() {
+    }
+
+    @BeforeAll
+    static void setUpClass() {
+        emf = EMF_Creator.createEntityManagerFactory(EMF_Creator.DbSelector.TEST, EMF_Creator.Strategy.DROP_AND_CREATE);
+        scraperFacade = ScraperFacade.getScraperFacade(emf);
+    }
+
+    @Test
+    void getAllApiDataInParallelWithQueue() throws ExecutionException, InterruptedException {
+        for (String json : scraperFacade.getAllApiData()) {
+            System.out.println(json);
+        }
+    }
+}
